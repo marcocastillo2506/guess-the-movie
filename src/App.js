7ec55a193a73dbaef5d6c2b619922048fab54372
marcocastillo2506 <marcocastillo2506@gmail.com>
2021-07-28 09:27:49 -0500

    guess the movie

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GuessTheMovie from "./components/GuessTheMovie"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <GuessTheMovie />
+    </div>
+  );
+}
+
+export default App;
